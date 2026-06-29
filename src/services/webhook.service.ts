34e9587c76668256ccd7ed767af8a15a83991039
Ammarabutalip <66907639+ammarkden@users.noreply.github.com>
2026-06-29 14:21:16 +0300

    fixes

--- src/services/webhook.service.ts
+++ src/services/webhook.service.ts
@@ -27,7 +27,7 @@ const pdfUrl = String(payload.pdfUrl)
     params.append("name", name);
 params.append("sum", total);
 if (payload.pdfUrl) {
-  params.append("pdf_url", String(payload.pdfUrl));
+  params.append("pdfUrl", String(payload.pdfUrl));
 }
     params.append("invoiceId", invoiceId);
 
