5b42c691423dc1e2aa8cac9fc41544d5e8a05e78
Ammarabutalip <66907639+ammarkden@users.noreply.github.com>
2026-07-05 13:11:44 +0300

    fixes

--- src/routes/webhook.routes.ts
+++ src/routes/webhook.routes.ts
@@ -72,7 +72,7 @@ const handleEvent =
 
       return reply.code(200).send({
         success: true,
-        message: "Webhook ignored for this client" + companyName,
+        message: "dd ignored for this client" + companyName,
       });
     }
 
